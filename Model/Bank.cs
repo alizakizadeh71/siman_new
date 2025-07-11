e7bd42ee4859b8199c6a770e0de6d8e2ccf958ff
AmirHossin Mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2025-07-11 11:50:08 +0330

    تبدیل موجودی به واحد بزرگتر

--- Model/Bank.cs
+++ Model/Bank.cs
@@ -11,7 +11,7 @@
 
         public string Name { get; set; }
 
-        public int Balance { get; set; }
+        public long Balance { get; set; }
         public virtual System.Collections.Generic.IList<BankAccount> BankAccounts { get; set; }
     }
 }
